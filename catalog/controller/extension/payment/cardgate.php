8600728a661dbf08b04a67175eec21819216e99d
richard <richard@cardgate.com>
2019-10-25 16:23:08 +0200

    New payment method: OnlineÜberweisen.

--- catalog/controller/extension/payment/cardgate.php
+++ catalog/controller/extension/payment/cardgate.php
@@ -17,7 +17,7 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class ControllerExtensionPaymentCardGate extends Controller {
-	var $version = '2.3.12';
+	var $version = '2.3.13';
 	
 	/**
 	 * Index action
